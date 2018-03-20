823c7a8e7a0c31e269ccb56da587edcaafa6fce8
mauro <mneagoe@agn.gov.ar>
2018-03-20 17:42:26 -0300

    cap9, completed

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mauro_ssj@hotmail.com']
+    POSTS_PER_PAGE = 3
